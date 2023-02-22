b65f459609bfcfc7f8974b0ae62f78385f90ee13
PasupulaS-Hexaware <109951310+PasupulaS-Hexaware@users.noreply.github.com>
2023-02-22 20:01:29 +0530

    Commented line in DataContext.cs

--- DotnetMysqlMraTest/Code/DotnetMysqlMraTest.Data/Repositories/DataContext.cs
+++ DotnetMysqlMraTest/Code/DotnetMysqlMraTest.Data/Repositories/DataContext.cs
@@ -2,7 +2,7 @@ using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
-using DotnetMysqlMraTest.Entities;
+// using DotnetMysqlMraTest.Entities.Entities;
 using MySqlConnector;
 
 namespace DotnetMysqlMraTest.Data.Repositories
